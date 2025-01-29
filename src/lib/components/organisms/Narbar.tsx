30a4ae98d918eab3a0f48310bd34f2cbbe3bc484
Julius Haring <info@juliusharing.com>
2025-01-29 11:29:32 +0100

    feat: add customer portal

--- src/lib/components/organisms/Narbar.tsx
+++ src/lib/components/organisms/Narbar.tsx
@@ -1,8 +1,13 @@
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+"use client";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
 import NavbarItem from "../atoms/NavbarItem";
 import { NAVBAR_CONTENT } from "@/app/admin/constants";
+import { useUser } from "@clerk/nextjs";
 
 const Navbar: React.FC = () => {
+  const user = useUser();
+  const email = user.user?.primaryEmailAddress;
+
   return (
     <nav className="h-screen fixed flex flex-col bg-blue-300 w-[12em] inset-shadow-sm  inset-shadow-blue-800">
       {NAVBAR_CONTENT.map((navbarItem, index) => (
@@ -10,6 +15,13 @@ const Navbar: React.FC = () => {
           {navbarItem.title}
         </NavbarItem>
       ))}
+
+      <NavbarItem
+        href={`https://billing.stripe.com/p/login/test_6oE6qx7tggif4XmcMM?prefilled_email=${email}`}
+        icon={UserCircleIcon}
+      >
+        Kundenportal
+      </NavbarItem>
     </nav>
   );
 };
