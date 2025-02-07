dc4a7b9cf9e71a1a87d77ae6fb775127deabbd52
Julius Haring <info@juliusharing.com>
2025-02-08 00:19:39 +0100

    feat: add mode to env for dev purposes

--- src/lib/utils/routes/auth.ts
+++ src/lib/utils/routes/auth.ts
@@ -2,6 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 import { Forbidden } from "./http-errors";
 
 export async function getUserId() {
+  if (process.env.MODE?.toLowerCase() === "dev") {
+    return "test";
+  }
+
   return auth().then((authValue) => {
     if (authValue.userId === null) {
       throw Forbidden("User not logged in");
