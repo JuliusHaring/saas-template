2a77d497b61ee58bcbb4a9d58a76bfb5721d9493
Julius Haring <info@juliusharing.com>
2025-02-08 15:18:26 +0100

    chore: update url for integrate src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { baseUrl } from "@/lib/utils/base-url";
+
 export default function Home() {
   return (
     <div>
       Todo: fill me
       <script
         async
-        src="http://localhost:3000/api/chatbot/integrate"
-        chatbot-id="61dcd944-45f0-40cb-b559-1fe53177b044"
-        api-url="http://localhost:3000"
+        src="/api/chatbot/integrate"
+        chatbot-id="39810b3e-6828-490c-9f5c-9cf02ee21792"
+        api-url={baseUrl}
       ></script>
     </div>
   );
