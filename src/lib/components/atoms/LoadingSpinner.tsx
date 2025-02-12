eefe4751cb99fd30f50e88d701fcf76f8677faae
Julius Haring <info@juliusharing.com>
2025-02-12 13:26:26 +0100

    style: reposition spinner default

--- src/lib/components/atoms/LoadingSpinner.tsx
+++ src/lib/components/atoms/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center">
       <div className="w-10 h-10 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
     </div>
   );
