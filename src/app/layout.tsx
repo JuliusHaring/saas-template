686232d98b120ce32c371dcd5028d10e8da387f3
Julius Haring <info@juliusharing.com>
2025-03-14 14:03:50 +0100

    style: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,15 +40,15 @@ export const metadata: Metadata = {
     capable: true,
   },
   icons: {
-    // icon: [
-    //   { url: "/favicon.ico", type: "image/x-icon" },
-    //   { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    //   { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    //   { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    //   { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-    //   { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
-    // ],
-    // shortcut: [{ url: "/favicon.ico", type: "image/x-icon" }],
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      //   { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      //   { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      //   { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      //   { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      //   { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    shortcut: [{ url: "/favicon.ico", type: "image/x-icon" }],
     // apple: [
     //   { url: "/apple-icon-57x57.png", sizes: "57x57", type: "image/png" },
     //   { url: "/apple-icon-60x60.png", sizes: "60x60", type: "image/png" },
