c3e33b4ec86dac55ae63829cf2b21a1af29c2b11
Julius Haring <info@juliusharing.com>
2025-03-14 14:27:10 +0100

    style: adopt landingpage navbar for mobile

--- src/lib/components/landingpage/organisms/LandingNavBar.tsx
+++ src/lib/components/landingpage/organisms/LandingNavBar.tsx
@@ -3,46 +3,86 @@ import NavbarItem from "@/lib/components/admin/atoms/NavbarItem";
 import Button from "@/lib/components/admin/molecules/Button";
 import NavBar from "@/lib/components/shared/organisms/NavBar";
 import { Logo } from "@/lib/components/shared/atoms/Logo";
+import { useState } from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 const LandingNavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <NavBar className="md:justify-between">
+    <NavBar className="lg:justify-between flex flex-col lg:flex-row items-center w-full px-4">
       <Logo className="cursor-default" />
+      {/* Mobile Menu Toggle */}
+      <button
+        className="lg:hidden absolute right-4 top-4 text-gray-800 focus:outline-none"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <Bars3Icon className="h-6 w-6" />
+      </button>
 
-      <div className="flex space-x-6">
-        <NavbarItem>
-          <ScrollLink to="eyecatcher" duration={500}>
-            Startseite
-          </ScrollLink>
-        </NavbarItem>
-        <NavbarItem>
-          <ScrollLink to="pricing" duration={500}>
-            Preise
-          </ScrollLink>
-        </NavbarItem>
-        <NavbarItem>
-          <ScrollLink to="howto" duration={500}>
-            Anleitung
-          </ScrollLink>
-        </NavbarItem>
-        <NavbarItem>
-          <ScrollLink to="testimonials" duration={500}>
-            Kundenbewertungen
-          </ScrollLink>
-        </NavbarItem>
+      {/* Menu Items */}
+      <div
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } lg:flex lg:items-center lg:space-x-6 w-full lg:w-auto mt-4 lg:mt-0 flex flex-col lg:flex-row space-y-4 lg:space-y-0`}
+      >
+        <div className="flex flex-col lg:flex-row lg:space-x-6 space-y-2 lg:space-y-0 w-full lg:w-auto text-center">
+          <NavbarItem>
+            <ScrollLink
+              to="eyecatcher"
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Startseite
+            </ScrollLink>
+          </NavbarItem>
+          <NavbarItem>
+            <ScrollLink
+              to="pricing"
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Preise
+            </ScrollLink>
+          </NavbarItem>
+          <NavbarItem>
+            <ScrollLink
+              to="howto"
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Anleitung
+            </ScrollLink>
+          </NavbarItem>
+          <NavbarItem>
+            <ScrollLink
+              to="testimonials"
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Kundenbewertungen
+            </ScrollLink>
+          </NavbarItem>
+          <NavbarItem>
+            <ScrollLink
+              to="code-example"
+              duration={500}
+              onClick={() => setMenuOpen(false)}
+            >
+              Code-Beispiel
+            </ScrollLink>
+          </NavbarItem>
+        </div>
 
-        <NavbarItem>
-          <ScrollLink to="code-example" duration={500}>
-            Code-Beispiel
-          </ScrollLink>
-        </NavbarItem>
-
-        <NavbarItem href="/auth/login">
-          <Button>Anmelden</Button>
-        </NavbarItem>
-        <NavbarItem href="/auth/signup">
-          <Button>Registrieren</Button>
-        </NavbarItem>
+        {/* Buttons */}
+        <div className="flex flex-col lg:flex-row lg:space-x-4 lg:space-y-0 space-y-2 mt-4 lg:mt-0 text-center">
+          <NavbarItem href="/auth/login">
+            <Button>Anmelden</Button>
+          </NavbarItem>
+          <NavbarItem href="/auth/signup">
+            <Button>Registrieren</Button>
+          </NavbarItem>
+        </div>
       </div>
     </NavBar>
   );
