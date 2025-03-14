36b27e1bc7638cd6129173bc8e2463ee2ad52809
Julius Haring <info@juliusharing.com>
2025-03-14 09:49:15 +0100

    feat: add video

--- src/lib/components/shared/organisms/TutorialVideo.tsx
+++ src/lib/components/shared/organisms/TutorialVideo.tsx
@@ -1,21 +1,18 @@
 export default function VideoAutoPlay() {
-    return (
-      <div className="relative w-full h-[400px] overflow-hidden">
-        {/* Top and Bottom Fade Effects */}
-        <div className="absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-white to-transparent pointer-events-none z-10"></div>
-        <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-t from-white to-transparent pointer-events-none z-10"></div>
-        
-        {/* YouTube Video Embed without controls */}
-        <iframe 
+  return (
+    <div className="relative flex justify-center items-center overflow-hidden">
+      <div className="border border-gray-300 md:w-3/4 sm:w-1/2 h-[calc(100%-200px)]">
+        <video
           className="w-full h-full object-cover"
-          src="https://www.youtube.com/embed/mSo_H-LWhwI?autoplay=1&mute=1&rel=0&loop=1&controls=0"
-          title="YouTube video player" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen>
-        </iframe>
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src="/videos/tutorial_trimmed_cut.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
