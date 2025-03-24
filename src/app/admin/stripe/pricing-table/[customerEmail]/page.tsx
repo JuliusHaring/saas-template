208f994179e26f890d39f67afc87790fbccc2283
Julius Haring <info@juliusharing.com>
2025-03-24 14:45:47 +0100

    refactor: add useEffect for pricing table

--- src/app/admin/stripe/pricing-table/[customerEmail]/page.tsx
+++ src/app/admin/stripe/pricing-table/[customerEmail]/page.tsx
@@ -1,18 +1,29 @@
 // @ts-nocheck
 "use client";
+import LoadingSpinner from "@/lib/components/admin/atoms/LoadingSpinner";
 import {
   NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID,
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
 } from "@/lib/utils/environment";
 import Script from "next/script";
+import { useEffect, useState } from "react";
 
-export default async function PricingTablePage({
+export default function PricingTablePage({
   params,
 }: {
   params: Promise<{ customerEmail: string }>;
 }) {
-  const rawEmail = (await params).customerEmail;
-  const customerEmail = decodeURIComponent(rawEmail);
+  const [email, setEmail] = useState<string>();
+
+  useEffect(() => {
+    const getEmail = async () => {
+      const rawEmail = (await params).customerEmail;
+      setEmail(decodeURIComponent(rawEmail));
+    };
+    getEmail();
+  }, []);
+
+  if (!email) return <LoadingSpinner />;
 
   return (
     <div className="w-full">
@@ -24,7 +35,7 @@ export default async function PricingTablePage({
       <stripe-pricing-table
         pricing-table-id={NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID}
         publishable-key={NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
-        customer-email={customerEmail}
+        customer-email={email}
       ></stripe-pricing-table>
     </div>
   );
